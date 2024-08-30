6fd15565bbaad5227a25bbc217835d963508c547
ziyad <mziyadbsk@gmail.com>
2024-08-30 15:12:29 +0300

    lesson 3

---
+++ datatypes.py
@@ -0,0 +1,8 @@
+a =36
+print("data tipe of a is",type(a))
+b =23.5
+print("data tipe of b is",type(b))
+c ="how is your day"
+print("data tipe of c is",type(c))
+d =True
+print("data tipe of d is",type(d))
---
+++ typecasting.py
@@ -0,0 +1,15 @@
+a =35
+d ="grapes"
+b =345.68
+c =True
+print("data type of a is",type(a))
+print("data type of d is",type(d))
+print("data type of b is",type(b))
+print("data type of c is",type(c))
+
+b=int(b)
+print("data type of b is",type(b))
+print(b)
+a=float(a)
+print("data type of a is",type(a))
+print(a)
